4600c92d5b4b8e35ec68575c68eac29b6db567f6
RuangAbreu <ruangabreu@hotmail.com>
2018-03-20 17:58:05 -0300

    Commit 20/03

--- ListaDinamica/app/src/main/java/com/example/alunos/listadinamica/model/Pessoa.java
+++ ListaDinamica/app/src/main/java/com/example/alunos/listadinamica/model/Pessoa.java
@@ -1,14 +1,47 @@
 package com.example.alunos.listadinamica.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by alunos on 19/03/18.
  */
 
-public class Pessoa {
+public class Pessoa implements Parcelable{
     private String nome;
     private String telefone;
     private int imagem;
 
+    protected Pessoa(Parcel in){
+        nome = in.readString();
+        telefone = in.readString();
+        imagem = in.readInt();
+    }
+
+    @Override
+    public int describeContents(){
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags){
+        dest.writeString(nome);
+        dest.writeString(telefone);
+        dest.writeInt(imagem);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Pessoa> CREATOR = new Parcelable.Creator<Pessoa>(){
+        @Override
+        public Pessoa createFromParcel(Parcel in){
+            return new Pessoa(in);
+        }
+        @Override
+        public Pessoa[] newArray(int size){
+            return new Pessoa[size];
+        }
+
+    }
     public Pessoa(String nome, String telefone, int idImagem){
         this.nome = nome;
         this.telefone = telefone;
