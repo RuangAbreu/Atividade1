b5fb97a3ac74c1ad1d74ffa0bc7f174407175e91
Dinegao <amaralrodriguesd@gmail.com>
2018-04-02 11:40:32 -0300

    Commit 02/04

--- QrCode/app/src/main/java/com/example/alunos/qr_code/Qr_Code.java
+++ QrCode/app/src/main/java/com/example/alunos/qr_code/Qr_Code.java
@@ -1,9 +1,64 @@
 package com.example.alunos.qr_code;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Qr_Code extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Qr_Code extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnScan;
+    private TextView lblNome, lblEndereco;
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr__code);
+
+        btnScan = findViewById(R.id.btnScan);
+        lblNome = findViewById(R.id.lblNome);
+        lblEndereco = findViewById(R.id.lblEndereco);
+
+        qrScan = new IntentIntegrator(this);
+        btnScan.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        qrScan.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null){
+
+        if (result.getContents() == null) {
+            Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+        } else {
+            try {
+                 JSONObject obj = new JSONObject(result.getContents());
+                lblNome.setText(obj.getString("name"));
+                lblEndereco.setText(obj.getString("address"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+            } else{
+                    super.onActivityResult(requestCode, resultCode, data);
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
